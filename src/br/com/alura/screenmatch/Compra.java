9b70883ddacc197c0d86211bc5b0afbd42c6b4a9
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-09 10:16:02 -0300

    Criando classe para teste do desafio, e ordem natural de acordo com o preço, usando a interface Comparable

--- src/br/com/alura/screenmatch/Compra.java
+++ src/br/com/alura/screenmatch/Compra.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch;
 
-public class Compra {
+public class Compra implements Comparable<Compra> {
     private String descricao;
     private double valor;
 
@@ -23,5 +23,10 @@ public class Compra {
     public String toString() {
         return "Compra: descrição = '" + descricao + '\'' +
                " valor = " + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
     }
 }
